394c4f66cb8d38ab48be9e4d3a1f79abc78bc472
Leonel Murillo <ljmurill@uci.edu>
2022-04-22 11:11:34 -0700

    database

--- backend/db/seeders/20220210223814-seedComments.js
+++ backend/db/seeders/20220210223814-seedComments.js
@@ -8,17 +8,7 @@ module.exports = {
 
       Example:
       */
-   return queryInterface.bulkInsert('Comments', [
-     {userId: 1, songId: 1, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 2, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 3, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 4, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 5, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 6, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 7, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 8, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-     {userId: 1, songId: 9, body: 'Favorite Song!', createdAt: new Date(), updatedAt: new Date()},
-   ], {});
+   return queryInterface.bulkInsert('Comments', null, {});
   },
 
   down: (queryInterface, Sequelize) => {
